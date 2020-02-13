aab6bda7e021fd01176c55640956e613aec2a262
Junbao Chen <Chen.Junbao@outlook.com>
2020-02-13 17:13:15 +0800

    Refactor and Encapsulate Code

--- test/test_cpm.py
+++ test/test_cpm.py
@@ -1,14 +1,14 @@
 import numpy as np
 import cv2
 import torch
-import torchvision.transforms.functional
+import torchvision.transforms.functional as F
 from preprocess.gen_data import gaussian_kernel
 
 
 def get_key_points(heatmap6, height, width):
 	"""
 	Get all key points from heatmap6.
-	:param heatmap6: The heatmap6 of CPM model.
+	:param heatmap6: The heatmap6 of CPM cpm.
 	:param height: The height of original image.
 	:param width: The width of original image.
 	:return: All key points of the person in the original image.
@@ -33,10 +33,10 @@ def get_key_points(heatmap6, height, width):
 	return key_points
 
 
-def draw_image(image_path, key_points):
+def draw_image(image, key_points):
 	"""
 	Draw limbs in the image.
-	:param image_path: The path of test image.
+	:param image: The test image.
 	:param key_points: The key points of the person in the test image.
 	:return: The painted image.
 	"""
@@ -58,7 +58,6 @@ def draw_image(image_path, key_points):
 	limbs = [[13, 12], [12, 9], [9, 10], [10, 11], [12, 8], [8, 7], [7, 6], [12, 3], [3, 4], [4, 5], [12, 2], [2, 1],
 	         [1, 0]]
 
-	image = cv2.imread(image_path)
 	# draw key points
 	for key_point in key_points:
 		x = key_point[0]
@@ -75,37 +74,38 @@ def draw_image(image_path, key_points):
 	return image
 
 
-model = torch.load('cpm.pth').cuda()
+if __name__ == "__main__":
+	model = torch.load('../model/best_cpm.pth').cuda()
 
-image_path = 'test_data/test1.jpg'
-image = cv2.imread(image_path)
-height, width, _ = image.shape
-image = np.asarray(cv2.imread(image_path), dtype=np.float32)
-image = cv2.resize(image, (368, 368), interpolation=cv2.INTER_CUBIC)
+	image_path = '../test_data/test.jpg'
+	image = cv2.imread(image_path)
+	height, width, _ = image.shape
+	image = np.asarray(image, dtype=np.float32)
+	image = cv2.resize(image, (368, 368), interpolation=cv2.INTER_CUBIC)
 
-# Normalize
-image -= image.mean()
-image = torchvision.transforms.functional.to_tensor(image)
+	# Normalize
+	image -= image.mean()
+	image = F.to_tensor(image)
 
-# Generate center map
-centermap = np.zeros((368, 368, 1), dtype=np.float32)
-kernel = gaussian_kernel(size_h=368, size_w=368, center_x=184, center_y=184, sigma=3)
-kernel[kernel > 1] = 1
-kernel[kernel < 0.01] = 0
-centermap[:, :, 0] = kernel
-centermap = torch.from_numpy(np.transpose(centermap, (2, 0, 1)))
+	# Generate center map
+	centermap = np.zeros((368, 368, 1), dtype=np.float32)
+	kernel = gaussian_kernel(size_h=368, size_w=368, center_x=184, center_y=184, sigma=3)
+	kernel[kernel > 1] = 1
+	kernel[kernel < 0.01] = 0
+	centermap[:, :, 0] = kernel
+	centermap = torch.from_numpy(np.transpose(centermap, (2, 0, 1)))
 
-image = torch.unsqueeze(image, 0).cuda()
-centermap = torch.unsqueeze(centermap, 0).cuda()
+	image = torch.unsqueeze(image, 0).cuda()
+	centermap = torch.unsqueeze(centermap, 0).cuda()
 
-model.eval()
-input_var = torch.autograd.Variable(image)
-center_var = torch.autograd.Variable(centermap)
+	model.eval()
+	input_var = torch.autograd.Variable(image)
+	center_var = torch.autograd.Variable(centermap)
 
-heat1, heat2, heat3, heat4, heat5, heat6 = model(input_var, center_var)
-key_points = get_key_points(heat6, height=height, width=width)
+	heat1, heat2, heat3, heat4, heat5, heat6 = model(input_var, center_var)
+	key_points = get_key_points(heat6, height=height, width=width)
 
-image = draw_image(image_path, key_points)
+	image = draw_image(cv2.imread(image_path), key_points)
 
-cv2.imshow('test image', image)
-cv2.waitKey(0)
+	cv2.imshow('test image', image)
+	cv2.waitKey(0)
---
+++ train_cpm/train.py
@@ -0,0 +1,104 @@
+from preprocess.gen_data import LSP_DATA
+from preprocess.Transformers import Compose, RandomCrop, RandomResized, TestResized
+from train_cpm.utils import AverageMeter
+from cpm import cpm
+import torch.utils.data.dataloader
+import torch.nn as nn
+
+if __name__ == "__main__":
+	training_dataset_path = 'F:/Python/PyCharmWorkspace/CPM/lspet/'
+	val_data_path = 'F:/Python/PyCharmWorkspace/CPM/lsp/'
+	model_save_path = '/model/cpm.pth'
+	best_model_path = '/model/best_cpm.pth'
+
+	criterion = nn.MSELoss().cuda()
+
+	model = cpm.CPM(k=14).cuda()
+
+	optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+	train_losses = AverageMeter()
+	val_losses = AverageMeter()
+	min_losses = 999.0
+
+	epoch = 0
+	while epoch < 20:
+		print('epoch ', epoch)
+		"""--------Train--------"""
+		# Training data
+		data = LSP_DATA('lspet', training_dataset_path, 8, Compose([RandomResized(), RandomCrop(368)]))
+		train_loader = torch.utils.data.dataloader.DataLoader(data, batch_size=8)
+		for j, data in enumerate(train_loader):
+			inputs, heatmap, centermap = data
+
+			inputs = inputs.cuda()
+			heatmap = heatmap.cuda(async=True)
+			centermap = centermap.cuda(async=True)
+
+			input_var = torch.autograd.Variable(inputs)
+			heatmap_var = torch.autograd.Variable(heatmap)
+			centermap_var = torch.autograd.Variable(centermap)
+
+			heat1, heat2, heat3, heat4, heat5, heat6 = model(input_var, centermap_var)
+
+			loss1 = criterion(heat1, heatmap_var)
+			loss2 = criterion(heat2, heatmap_var)
+			loss3 = criterion(heat3, heatmap_var)
+			loss4 = criterion(heat4, heatmap_var)
+			loss5 = criterion(heat5, heatmap_var)
+			loss6 = criterion(heat6, heatmap_var)
+
+			loss = loss1 + loss2 + loss3 + loss4 + loss5 + loss6
+			train_losses.update(loss.item(), inputs.size(0))
+
+			optimizer.zero_grad()
+			loss.backward()
+			optimizer.step()
+		print('Train Loss: ', train_losses.avg)
+		torch.save(model, model_save_path)
+
+		'''--------Validation--------'''
+		# Validation
+		print('-----------Validation-----------')
+		# Validation data
+		data = LSP_DATA('lsp', val_data_path, 8, Compose([TestResized(368)]))
+		val_loader = torch.utils.data.dataloader.DataLoader(data, batch_size=8)
+
+		model.eval()
+
+		for j, data in enumerate(val_loader):
+			inputs, heatmap, centermap = data
+
+			inputs = inputs.cuda()
+			heatmap = heatmap.cuda(async=True)
+			centermap = centermap.cuda(async=True)
+
+			input_var = torch.autograd.Variable(inputs)
+			heatmap_var = torch.autograd.Variable(heatmap)
+			centermap_var = torch.autograd.Variable(centermap)
+
+			heat1, heat2, heat3, heat4, heat5, heat6 = model(input_var, centermap_var)
+
+			loss1 = criterion(heat1, heatmap_var)
+			loss2 = criterion(heat2, heatmap_var)
+			loss3 = criterion(heat3, heatmap_var)
+			loss4 = criterion(heat4, heatmap_var)
+			loss5 = criterion(heat5, heatmap_var)
+			loss6 = criterion(heat6, heatmap_var)
+
+			loss = loss1 + loss2 + loss3 + loss4 + loss5 + loss6
+			val_losses.update(loss.item(), inputs.size(0))
+
+			optimizer.zero_grad()
+			loss.backward()
+			optimizer.step()
+
+		print('Validation Loss: ', val_losses.avg)
+		if val_losses.avg < min_losses:
+			# Save best cpm
+			torch.save(model, best_model_path)
+			min_losses = val_losses.avg
+
+		model.train()
+
+		epoch += 1
