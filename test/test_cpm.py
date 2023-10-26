005278f206cfb1016b2cc2b44e95a56e8e09a4fd
umej <50549192+umej@users.noreply.github.com>
2023-10-26 15:18:47 +0900

    Update test_cpm.py

--- test/test_cpm.py
+++ test/test_cpm.py
@@ -106,7 +106,7 @@ if __name__ == "__main__":
 	key_points = get_key_points(heat6, height=height, width=width)
 
 	heat6img = np.asarray(heat6.cpu().data)[0]
-	printf(heat6img)
+	print(heat6img)
 	cv2.imwrite(image_path.rsplit('.', 1)[0] + '_heat6.png', heat6img[0,:])
 	
 	image = draw_image(cv2.imread(image_path), key_points)
