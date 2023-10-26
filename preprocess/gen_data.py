a9790308c82414fb5b4a038036ecbf889ae06871
umej <50549192+umej@users.noreply.github.com>
2023-10-26 14:28:37 +0900

    Update gen_data.py

--- preprocess/gen_data.py
+++ preprocess/gen_data.py
@@ -52,8 +52,7 @@ def read_mat(mode, path, image_list):
 	center_point_list = []
 	scale_list = []
 
-#	for i in range(len(image_list)):
-	for i in range(32):
+	for i in range(len(image_list)):
 		image = cv2.imread(image_list[i])
 
 		h = image.shape[0]
